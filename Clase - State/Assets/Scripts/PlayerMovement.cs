adabbeea20c6557fb5f1cccbc5c8deca868a1c6d
Nicolas Martin Rodriguez <rodriguezbohl@gmail.com>
2025-08-26 19:35:23 -0300

    First Commit - Player movement

---
+++ Clase - State/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f; // Velocidad del jugador
+
+    private Rigidbody rb; // Referencia al Rigidbody
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; // Para que no se caiga de costado
+    }
+
+    void FixedUpdate()
+    {
+        // Capturamos input (WASD / Flechas)
+        float x = Input.GetAxisRaw("Horizontal"); // A/D o ←/→
+        float z = Input.GetAxisRaw("Vertical");   // W/S o ↑/↓
+
+        // Creamos un vector de movimiento (solo XZ)
+        Vector3 move = new Vector3(x, 0f, z).normalized;
+
+        // Aplicamos movimiento multiplicado por velocidad
+        rb.velocity = move * moveSpeed;
+    }
+}
