bf14a351c8f21a09910b16280f244c4708157c73
Nicolas Martin Rodriguez <rodriguezbohl@gmail.com>
2025-09-09 19:31:52 -0300

    [ADD] TypeOff pattern to SO_Enemy Scriptable object
    *Added TypeOff pattern to SO_Enemy Scriptable object
    * Modified monsters prefabs for new properties of Scriptable object - added type.
    * included virtual methods for attack and overrides for specific class type

--- Clase - State/Assets/Scripts/ScriptableObjectsScripts/SO_Enemy.cs
+++ Clase - State/Assets/Scripts/ScriptableObjectsScripts/SO_Enemy.cs
@@ -8,5 +8,37 @@ public class SO_Enemy : ScriptableObject
     public float maxHealth;
     public float baseDamage;
     public Material baseMaterial;
+    public string enemyType;
 
+    public virtual void Attack()
+    {
+        Debug.Log("Atacando y realizando " + baseDamage + " de daño");
+    }
+}
+
+public class Vampire : SO_Enemy
+{ 
+    public override void Attack()
+    {
+        Debug.Log("Vampire atacando...");
+        base.Attack();
+    }
+}
+
+public class Zombie : SO_Enemy
+{
+    public override void Attack()
+    {
+        Debug.Log("Zombie atacando...");
+        base.Attack();
+    }
+}
+
+public class Ghost : SO_Enemy
+{
+    public override void Attack()
+    {
+        Debug.Log("Ghost atacando...");
+        base.Attack();
+    }
 }
