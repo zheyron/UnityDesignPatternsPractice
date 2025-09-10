e0a2c1c3af4e98a2c167d9d7c969423e95cbb7e0
Nicolas Martin Rodriguez <rodriguezbohl@gmail.com>
2025-09-09 21:05:01 -0300

    [ADD] Agregar prefabs para abstract factory - part1
     Agregar prefabs para abstract factory - part1

---
+++ Clase - State/Assets/Scripts/Instanciador.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Abstract Factory
+public class Instanciador : MonoBehaviour
+{
+    
+    [SerializeField] private EnemyFactory factory;
+    [SerializeField] private Transform _spawnPoint;
+
+    private void Update()
+    {
+        //
+    }
+
+}
